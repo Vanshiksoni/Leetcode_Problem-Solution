e4e9be1227e8c481eaf6c4c95cb41c8b7a7fc146
Vanshik Soni <bobbysoni1802@gmail.com>
2026-07-09 11:59:46 +0530

    Time: 58 ms (82.17%) | Memory: 76.3 MB (84.90%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
@@ -1,60 +1,26 @@
 class Solution {
-    public static int[] findPSE(int[] heights) {
-        int n = heights.length;
-        int[] pse = new int[n];
-        Stack<Integer> st = new Stack<>();
-
-        for (int i = 0; i < n; i++) {
-
-            while (!st.isEmpty() && heights[st.peek()] >= heights[i]) {
-                st.pop();
-            }
-
-            if (st.isEmpty())
-                pse[i] = -1;
-            else
-                pse[i] = st.peek();
-
-            st.push(i);
-        }
-
-        return pse;
-    }
-
-    // Next Smaller Element
-    public static int[] findNSE(int[] heights) {
-        int n = heights.length;
-        int[] nse = new int[n];
-        Stack<Integer> st = new Stack<>();
-
-        for (int i = n - 1; i >= 0; i--) {
-
-            while (!st.isEmpty() && heights[st.peek()] >= heights[i]) {
-                st.pop();
-            }
-
-            if (st.isEmpty())
-                nse[i] = n;
-            else
-                nse[i] = st.peek();
-
-            st.push(i);
-        }
-
-        return nse;
-    }
+    
     public int largestRectangleArea(int[] heights) {
         int n = heights.length;
         Stack<Integer> st = new Stack<>();
         int ans = 0;
-        int[] nse = findNSE(heights);
-        int[] pse = findPSE(heights);
-        
-        for(int i =0; i< n; i++){
-            ans = Math.max(ans, heights[i] * (nse[i] - pse[i] -1)); 
+    
+        for (int i = 0; i <= n; i++) {
+    
+            while (!st.isEmpty() &&
+                    (i == n || heights[st.peek()] >= heights[i])) {
+    
+                int height = heights[st.pop()];
+    
+                int pse = st.isEmpty() ? -1 : st.peek();
+                int width = i - pse - 1;
+    
+                ans = Math.max(ans, height * width);
+            }
+    
+            st.push(i);
         }
-        
-
+    
         return ans;
     }
 }
