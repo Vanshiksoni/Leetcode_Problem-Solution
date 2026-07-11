abe7fbde9d5206a0d6a629b2ef4cefecf950c1db
Vanshik Soni <bobbysoni1802@gmail.com>
2026-07-11 11:43:07 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (69.08%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.java
+++ 678-valid-parenthesis-string/valid-parenthesis-string.java
@@ -1,34 +1,30 @@
 class Solution {
 
-    Boolean[][] dp;
 
     public boolean checkValidString(String s) {
-        dp = new Boolean[s.length() + 1][s.length() + 1];
-        return solve(s, 0, 0);
-    }
-
-    boolean solve(String s, int ind, int count) {
-
-        if (count < 0)
-            return false;
-
-        if (ind == s.length())
-            return count == 0;
-
-        if (dp[ind][count] != null)
-            return dp[ind][count];
-
-        char ch = s.charAt(ind);
-
-        if (ch == '(')
-            return dp[ind][count] = solve(s, ind + 1, count + 1);
-
-        if (ch == ')')
-            return dp[ind][count] = solve(s, ind + 1, count - 1);
-
-        return dp[ind][count] =
-                solve(s, ind + 1, count + 1)
-             || solve(s, ind + 1, count - 1)
-             || solve(s, ind + 1, count);
+        int minO=0;
+        int maxO=0;
+        for(char ch: s.toCharArray()){
+            if(ch=='('){
+                minO++;
+                maxO++;
+            }else if(ch==')'){
+                minO--;
+                maxO--;
+            }else{
+                minO--;
+                maxO++;
+            }
+            if(minO<0){
+                minO=0;
+            }
+            if(maxO<0){
+                return false;
+            }
+        }
+        if(minO==0){
+            return true;
+        }
+        return false;
     }
 }
