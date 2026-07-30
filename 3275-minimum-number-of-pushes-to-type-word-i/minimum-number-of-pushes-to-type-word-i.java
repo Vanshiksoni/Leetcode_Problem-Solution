847f6ce25933af5b0313e08c4ee4ef893aa684d3
Vanshik Soni <bobbysoni1802@gmail.com>
2026-07-30 12:12:58 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (47.13%) - LeetSync

--- 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.java
+++ 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.java
@@ -2,24 +2,11 @@ import java.util.Arrays;
 
 class Solution {
     public int minimumPushes(String word) {
-        int[] freq = new int[26];
+        int n = word.length();
+        int rem = n % 8;
+        int comp = n / 8;
 
-        for (char c : word.toCharArray()) {
-            freq[c - 'a']++;
-        }
-
-        Arrays.sort(freq);
-
-        int ans = 0;
-        int presses = 1;
-
-        for (int i = 25, cnt = 0; i >= 0 && freq[i] > 0; i--, cnt++) {
-            if (cnt == 8) presses = 2;
-            else if (cnt == 16) presses = 3;
-            else if (cnt == 24) presses = 4;
-
-            ans += freq[i] * presses;
-        }
+        int ans = 8 * comp * (comp + 1) / 2 + (comp + 1) * rem;
 
         return ans;
     }
