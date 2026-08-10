3a44dc6019fe7b1aedb44c4d0e60e4de4d3c09aa
Vanshik Soni <bobbysoni1802@gmail.com>
2026-08-10 11:32:25 +0530

    Time: 16 ms (59.53%) | Memory: 42.6 MB (60.84%) - LeetSync

---
+++ 1617-stone-game-iv/stone-game-iv.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean winnerSquareGame(int n) {
+        boolean[] dp = new boolean[n + 1];
+
+        // dp[i] = true if the current player can win with i stones
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j * j <= i; j++) {
+                // If removing j*j leaves a losing position,
+                // current player wins.
+                if (!dp[i - j * j]) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+
+        return dp[n];
+    }
+}
