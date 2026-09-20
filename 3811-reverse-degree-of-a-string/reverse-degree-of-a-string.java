18338885a4de43a656f1e1f44ddfb1675b16849a
Vanshik Soni <bobbysoni1802@gmail.com>
2026-09-20 07:35:10 +0530

    Time: 1 ms (100.00%) | Memory: 43.9 MB (81.16%) - LeetSync

---
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int reverseDegree(String s) {
+        int ans = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            int reversedPos = 26 - (s.charAt(i) - 'a');
+            ans += reversedPos * (i + 1);
+        }
+
+        return ans;
+    }
+}
